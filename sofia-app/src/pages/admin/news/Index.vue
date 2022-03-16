85feb487a4f255df25738e3c54aef6f0d75460b4
Valeriy V Nepluev <greenet2007@gmail.com>
2022-03-16 21:59:19 +0300

    feat: add news page concept

--- sofia-app/src/pages/admin/news/Index.vue
+++ sofia-app/src/pages/admin/news/Index.vue
@@ -1,5 +1,24 @@
 <template>
-	<h1>Admin Protected Page</h1>
+	<div class="q-pa-md q-gutter-sm text-center">
+		<div class="q-px-lg q-pb-md">
+			<q-timeline color="primary">
+				<br />
+				<q-timeline-entry>
+					<template v-slot:title class="bold">Event Title</template>
+					<template v-slot:subtitle>February 22, 1986</template>
+
+					<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+				</q-timeline-entry>
+
+				<q-timeline-entry icon="tsunami">
+					<template v-slot:title>Event Title</template>
+					<template v-slot:subtitle>February 21, 1986</template>
+
+					<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+				</q-timeline-entry>
+			</q-timeline>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -8,3 +27,10 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+.news-card {
+	width: 100%;
+	max-width: 250px;
+}
+</style>
